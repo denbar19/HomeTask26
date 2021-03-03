8f904fd01cb5ebcafcde5da639617a56b126da1a
denbar <denisenko924@gmail.com>
2021-03-03 23:49:44 +0200

    some dependencies and settings

---
+++ src/main/java/MyHttpServlet.java
@@ -0,0 +1,9 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyHttpServlet extends HttpServlet {
+
+}
--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,3 +1,34 @@
+import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
-public class MyServlet {
+
+public class MyServlet implements Servlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
